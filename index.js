21b8c475632c017dfdc825c031ac99a2d05b8cd3
Krzysztof Biskup <k.biskup.ns@gmail.com>
2019-11-07 20:44:13 +0100

    ready for review

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer();
-var data = '';
 
 fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
     if (err) throw err;
+
+    var array = data.split(',')
+    var stringfyData = array.join('<br>');
+
     server.on('request', function (request, response) {
         response.setHeader("Content-Type", "text/html; charset=utf-8");
-            if (request.method === 'GET' && request.url === '/t') {
-                response.write('<body>');
-                response.write(data);
-                response.write('</body>');
-                response.end();
+        
+        if (request.method === 'GET' && request.url === '/t') {
+            response.write('<body>');
+            response.write(stringfyData);
+            response.write('</body>');
+            response.end();
         }   else {
-                response.statusCode = 404;
-                response.write('<h1>404: Zła ścieżka!</h1>');
-                response.end();
-            }
+            response.statusCode = 404;
+            response.write('<h1>404: Zła ścieżka!</h1>');
+            response.end();
+        }
     });
 });
 
