95f24ae149a20dceace9a5dd5018279a90b3db02
Krzysztof Biskup <k.biskup.ns@gmail.com>
2019-11-07 12:06:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+var http = require('http');
 var fs = require('fs');
-var StatMode = require('stat-mode');
 
-fs.readdir('./', 'utf-8', function(err, data) {
-    console.log(data);
-    fs.writeFile('./tekst.txt', data, function(err){
-        if (err) throw err;
-        console.log('Zapisano');
-    })
-})
+var server = http.createServer();
+var data = '';
+
+fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+    if (err) throw err;
+    server.on('request', function (request, response) {
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
+            if (request.method === 'GET' && request.url === '/t') {
+                response.write('<body>');
+                response.write(data);
+                response.write('</body>');
+                response.end();
+        }   else {
+                response.statusCode = 404;
+                response.write('<h1>404: Zła ścieżka!</h1>');
+                response.end();
+            }
+    });
+});
+
+server.listen(8080);
